9157c6be197b1b7a3df7107d82ae61284aa646cf
Marcel Medina <marcel.net@gmail.com>
2026-02-04 23:41:41 +1000

    Add initial project files and implement RestaurantService with data management

---
+++ LunchTime/LunchTimeMCP/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport();
+
+await builder.Build().RunAsync();
